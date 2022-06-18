779e16a5216d44fb92bb94728f555de1eda9c738
asoikaw15 <krteammain@gmail.com>
2022-06-18 20:26:03 +0900

    fixed typo

--- models/users.js
+++ models/users.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   discordId: String,
   section: Number,
-  dicordName: {
+  discordName: {
     unique: false,
     type: String,
   },
